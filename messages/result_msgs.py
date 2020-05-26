76b6b9784701cc38d88068846324883ed82af15b
The Piano Dentist <jbknight07@gmail.com>
2020-05-26 20:12:06 +0100

    use dataclasses over namedtuples

--- messages/result_msgs.py
+++ messages/result_msgs.py
@@ -1,22 +1,122 @@
+import uuid
 from collections import namedtuple
+from dataclasses import dataclass, field
+
 # https://realpython.com/python-data-classes/
 
 # Subscriptions
-SubTeam = namedtuple('SubTeam', 'toggle')
-SubCompetition = namedtuple('SubTeam', 'all, sub_competition_ids, unsub_competition_ids')
+from datetime import datetime
+from typing import List, Tuple, Optional
+
+
+@dataclass
+class SubTeam:
+    toggle: bool
+
+
+@dataclass
+class SubCompetition:
+    all: bool
+    # Because cant have mutable [] as default
+    sub_competition_ids: Optional[List[uuid]] = None
+    unsub_competition_ids: Optional[List[uuid]] = None
 
 
 # Insert/Updates
-Competition = namedtuple('Competition', 'competition_id, name, meta, timespan, series')
-Series = namedtuple('Series', 'series_id, name, meta, timespan, matches, team_results, competition_id')
-Match = namedtuple('Match', 'match_id, name, meta, timespan, team_match_results, player_results, series_id')
-TeamSeriesResult = namedtuple('TeamSeriesResult', 'team_id, series_id, meta, result')
-TeamMatchResult = namedtuple('TeamMatchResult', 'team_id, match_id, meta, result')
-PlayerResult = namedtuple('PlayerResult', 'player_id, match_id, meta, result')
+@dataclass
+class TeamSeriesResult:
+    series_id: uuid
+    team_id: uuid
+    result: str
+    meta: dict = field(default_factory=dict)
 
-Team = namedtuple('Team', 'team_id, meta, names')
-TeamName = namedtuple('TeamName', 'team_id, name, timespan')
-TeamPlayer = namedtuple('TeamPlayer', 'team_id, player_id, timespan')
-Player = namedtuple('Player', 'player_id, meta, names, positions')
-PlayerName = namedtuple('PlayerName', 'player_id, name, timespan')
-PlayerPosition = namedtuple('PlayerPosition', 'player_id, position, timespan')
+
+@dataclass
+class TeamMatchResult:
+    match_id: uuid
+    team_id: uuid
+    result: str
+    meta: dict = field(default_factory=dict)
+
+
+@dataclass
+class PlayerResult:
+    match_id: uuid
+    player_id: uuid
+    result: dict
+    meta: dict = field(default_factory=dict)
+
+
+@dataclass
+class Match:
+    match_id: uuid
+    name: str
+    # Timespans are str's as can't implicitly json convert datetimes (could make a custom serializer though)
+    timespan: Tuple[str, str]
+    series_id: Optional[uuid] = None
+    meta: dict = field(default_factory=dict)
+    player_results: Optional[List[PlayerResult]] = None
+    team_match_results: Optional[List[TeamMatchResult]] = None
+
+
+@dataclass
+class Series:
+    series_id: uuid
+    name: str
+    timespan: Tuple[str, str]
+    competition_id: Optional[uuid] = None
+    meta: dict = field(default_factory=dict)
+    matches: Optional[List[Match]] = None
+    team_results: Optional[List[TeamSeriesResult]] = None
+
+
+@dataclass
+class Competition:
+    competition_id: uuid
+    name: str
+    timespan: Tuple[str, str]
+    meta: dict = field(default_factory=dict)
+    series: Optional[List[Series]] = None
+
+
+@dataclass
+class PlayerPosition:
+    position: str
+    timespan: Tuple[str, str]
+    player_id: Optional[uuid] = None
+
+
+@dataclass
+class PlayerName:
+    name: str
+    timespan: Tuple[str, str]
+    player_id: Optional[uuid] = None
+
+
+@dataclass
+class Player:
+    player_id: uuid
+    meta: dict = field(default_factory=dict)
+    names: Optional[List[PlayerName]] = None
+    positions: Optional[List[PlayerPosition]] = None
+
+
+@dataclass
+class TeamName:
+    name: str
+    timespan: Tuple[str, str]
+    team_id: Optional[uuid] = None
+
+
+@dataclass
+class Team:
+    team_id: uuid
+    meta: dict = field(default_factory=dict)
+    names: Optional[List[TeamName]] = None
+
+
+@dataclass
+class TeamPlayer:
+    team_id: uuid
+    player_id: uuid
+    timespan: Tuple[str, str]
