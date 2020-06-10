32d1af86a545d9031136305f88b4cfcfa54219e7
Charles Taylor <charlesastaylor@gmail.com>
2020-06-10 01:59:21 +0100

    "fix" leaderboard messages

--- messages/leaderboard_msgs.py
+++ messages/leaderboard_msgs.py
@@ -37,16 +37,31 @@ class Stat:
     league_id: Optional[str] = None
 
 
+# pub struct ApiLeaderboardLatest {
+#     pub leaderboard_id: Uuid,
+#     pub league_id:      Uuid,
+#     pub name:           String,
+#     pub meta: serde_json::Value,
+#     pub leaderboard: Vec<ApiLatestStat>,
+# }
 @dataclass
 class Leaderboard:
     leaderboard_id: str
     league_id: str
     name: str
-    timespan: Tuple[str, str]
+    timespan: Optional[Tuple[str, str]] = None
     meta: dict = field(default_factory=dict)
-    stats: Optional[List[Stat]] = None
+    leaderboard: Optional[List[Stat]] = None
 
 
+# pub struct ApiLatestStat {
+#     #[sql_type = "sql_types::Uuid"]
+#     pub player_id: Uuid,
+#     #[sql_type = "sql_types::Uuid"]
+#     pub leaderboard_id: Uuid,
+#     #[sql_type = "sql_types::Double"]
+#     pub points: f64,
+# }
 @dataclass
 class LatestStat:
     player_id: str
